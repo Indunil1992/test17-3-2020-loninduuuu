c87a789047cecb827acb2a57606bb96cfd5f5c48
Indunil1992 <indunil@adroitlogic.com>
2020-03-17 11:41:55 +0530

    1

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.test1732020Loninduuuu = require("./test17-3-2020-loninduuuu.js").handler;
